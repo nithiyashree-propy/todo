7b46496e378346086cde078c7babb0bee38dc826
nithiyashree-propy <144881143+nithiyashree-propy@users.noreply.github.com>
2023-10-30 23:25:11 +0530

    first commit

---
+++ todo/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # add task
+    path('addTask/', views.addTask, name='addTask'),
+    # mark as done
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    # mark as undone
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+
+    # Edit features
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+
+    # delete task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
+]
